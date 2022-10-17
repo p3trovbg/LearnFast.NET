43402956975689a8cd76927178a64cedae74e8a7
George <p3trovsit@gmail.com>
2022-10-17 18:53:08 +0300

    Creating difficulty service and a drop list about "create" and "edit" views

---
+++ Services/LearnFast.Services.Data/DifficultyService.cs
@@ -0,0 +1,17 @@
+﻿namespace LearnFast.Services.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using LearnFast.Data.Models.Enums;
+
+    public class DifficultyService : IDifficultyService
+    {
+        public Dictionary<int, string> GetAll()
+        {
+            return Enum.GetValues(typeof(Difficulty))
+                .Cast<Difficulty>()
+                .ToDictionary(t => (int)t, t => t.ToString());
+        }
+    }
+}
---
+++ Services/LearnFast.Services.Data/IDifficultyService.cs
@@ -0,0 +1,10 @@
+﻿using LearnFast.Data.Models.Enums;
+using System.Collections.Generic;
+
+namespace LearnFast.Services.Data
+{
+    public interface IDifficultyService
+    {
+        Dictionary<int, string> GetAll();
+    }
+}
