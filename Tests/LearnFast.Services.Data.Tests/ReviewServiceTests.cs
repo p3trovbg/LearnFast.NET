a4ee95e3bcecd9ba8f64e3d82965019ff318a6df
George <p3trovsit@gmail.com>
2022-12-03 12:30:29 +0200

    Testing all review service

--- Tests/LearnFast.Services.Data.Tests/ReviewServiceTests.cs
+++ Tests/LearnFast.Services.Data.Tests/ReviewServiceTests.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Threading.Tasks;
     using LearnFast.Common;
@@ -123,8 +124,236 @@
         {
             var targetReview = new EditReviewViewModel { Id = -1, Content = "TestUpdate", Rating = 6 };
             var service = new ReviewService(this.repository.Object, this.filterCourse.Object, this.Mapper);
-         
+
             var ex = await Assert.ThrowsAsync<ArgumentException>(async () => await service.Edit(targetReview));
+            Assert.Equal(GlobalExceptions.DoesNotExistReview, ex.Message);
+        }
+
+        [Theory]
+        [InlineData(5)]
+        public async Task GetAllReviewsByCourseShouldSelectsReviewsByValidCourse(int courseId)
+        {
+            var allReviews = GetReviews();
+            var model = new ReviewListViewModel();
+
+            var mappedReviews = this.Mapper.Map<List<ReviewViewModel>>(allReviews);
+
+            this.filterCourse
+              .Setup(x => x.GetCourseByIdAsync<BaseCourseViewModel>(It.IsAny<int>())).ReturnsAsync(new BaseCourseViewModel { Id = courseId });
+
+            model.Reviews = mappedReviews;
+            model.CourseId = courseId;
+
+            var service = new ReviewService(this.repository.Object, this.filterCourse.Object, this.Mapper);
+
+            await service.GetAllReviewsByCourse(model);
+
+            Assert.All(model.Reviews, x => Assert.Equal(courseId, x.CourseId));
+        }
+
+        [Theory]
+        [InlineData(5)]
+        public async Task GetAllReviewsByCourseShouldReturnOnly9ReviewsByPage(int courseId)
+        {
+            var allReviews = GetReviews();
+            var model = new ReviewListViewModel();
+
+            var mappedReviews = this.Mapper.Map<List<ReviewViewModel>>(allReviews);
+
+            this.filterCourse
+              .Setup(x => x.GetCourseByIdAsync<BaseCourseViewModel>(It.IsAny<int>())).ReturnsAsync(new BaseCourseViewModel { Id = courseId });
+
+            model.Reviews = mappedReviews;
+            model.CourseId = courseId;
+
+            var service = new ReviewService(this.repository.Object, this.filterCourse.Object, this.Mapper);
+
+            await service.GetAllReviewsByCourse(model);
+
+            Assert.Equal(9, model.Reviews.Count());
+            Assert.All(model.Reviews, x => Assert.Equal(courseId, x.CourseId));
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        public async Task GetAllReviewsByCourseShouldThrowsExceptionIfCourseDoesNotExist(int courseId)
+        {
+            var model = new ReviewListViewModel() { CourseId = courseId };
+
+            this.filterCourse
+              .Setup(x => x.GetCourseByIdAsync<BaseCourseViewModel>(It.IsAny<int>())).ReturnsAsync((BaseCourseViewModel)null);
+
+            var service = new ReviewService(this.repository.Object, this.filterCourse.Object, this.Mapper);
+
+            var ex = await Assert.ThrowsAsync<ArgumentException>(async () => await service.GetAllReviewsByCourse(model));
+            Assert.Equal(GlobalExceptions.CourseDoesNotExistExceptionMessage, ex.Message);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(5)]
+        public async Task GetReviewByIdShouldReturnReview(int reviewId)
+        {
+            var expectedReview = GetReviews().FirstOrDefault(x => x.Id == reviewId);
+
+            var service = new ReviewService(this.repository.Object, this.filterCourse.Object, this.Mapper);
+
+            var result = await service.GetReviewById<ReviewViewModel>(reviewId);
+
+            Assert.Equal(expectedReview.Id, result.Id);
+            Assert.Equal(expectedReview.Content, result.Content);
+        }
+
+        [Theory]
+        [InlineData(-50)]
+        [InlineData(-1)]
+        public async Task GetReviewByInvalidIdShouldThrowsException(int reviewId)
+        {
+            var expectedReview = GetReviews().FirstOrDefault(x => x.Id == reviewId);
+
+            var service = new ReviewService(this.repository.Object, this.filterCourse.Object, this.Mapper);
+
+            var ex = await Assert.ThrowsAsync<NullReferenceException>(async () => await service.GetReviewById<ReviewViewModel>(reviewId));
+            Assert.Equal(GlobalExceptions.DoesNotExistReview, ex.Message);
+        }
+
+        [Fact]
+        public async Task GetReviewsCountShouldReturnCorrectNumber()
+        {
+            var expectedReviewsCount = GetReviews().Count();
+            var service = new ReviewService(this.repository.Object, this.filterCourse.Object, this.Mapper);
+
+            var result = await service.GetReviewsCountAsync();
+            Assert.Equal(expectedReviewsCount, result);
+            this.repository.Verify(m => m.AllAsNoTracking(), Times.Once());
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(3)]
+        public async Task GetSelectedReviewByCourseShouldReturnSelectedReviews(int courseId)
+        {
+            this.filterCourse
+              .Setup(x => x.GetCourseByIdAsync<BaseCourseViewModel>(It.IsAny<int>()))
+              .ReturnsAsync(new BaseCourseViewModel { Id = courseId });
+
+            var service = new ReviewService(this.repository.Object, this.filterCourse.Object, this.Mapper);
+
+            var result = await service.GetSelectedReviewsByCourse<ReviewViewModel>(courseId);
+
+            Assert.All(result, x => Assert.True(x.IsSelected));
+            this.repository.Verify(m => m.AllAsNoTracking(), Times.Once());
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(3)]
+        public async Task GetSelectedReviewByInvalidCourseShouldThrowsException(int courseId)
+        {
+            this.filterCourse
+              .Setup(x => x.GetCourseByIdAsync<BaseCourseViewModel>(It.IsAny<int>()))
+              .ReturnsAsync((BaseCourseViewModel)null);
+
+            var service = new ReviewService(this.repository.Object, this.filterCourse.Object, this.Mapper);
+
+            var ex = await Assert.ThrowsAsync<ArgumentException>(async () => await service.GetSelectedReviewsByCourse<ReviewViewModel>(courseId));
+            Assert.Equal(GlobalExceptions.CourseDoesNotExistExceptionMessage, ex.Message);
+        }
+
+        [Theory]
+        [InlineData(4)]
+        public async Task SelectingReviewByCourseShouldMakesIsSelectedTrueIfIsFalse(int reviewId)
+        {
+            var reviews = GetReviews();
+            var expected = reviews.FirstOrDefault(x => x.Id == reviewId);
+            var model = new SelectingReviewViewModel() { CourseId = expected.CourseId, ReviewId = reviewId, IsSelected = expected.IsSelected };
+
+            this.repository.Setup(x => x.All()).Returns(reviews.BuildMock());
+
+            this.filterCourse
+               .Setup(x => x.GetCourseByIdAsync<BaseCourseViewModel>(It.IsAny<int>()))
+               .ReturnsAsync(new BaseCourseViewModel { Id = expected.CourseId });
+
+            var service = new ReviewService(this.repository.Object, this.filterCourse.Object, this.Mapper);
+            await service.Selecting(model);
+
+            Assert.True(expected.IsSelected);
+            this.repository.Verify(m => m.All(), Times.Once());
+            this.repository.Verify(m => m.SaveChangesAsync(), Times.Once());
+        }
+
+        [Theory]
+        [InlineData(14)]
+        public async Task SelectingReviewByCourseShouldMakesIsSelectedFalseIfIsTrue(int reviewId)
+        {
+            var reviews = GetReviews();
+            var expected = reviews.FirstOrDefault(x => x.Id == reviewId);
+            var model = new SelectingReviewViewModel() { CourseId = expected.CourseId, ReviewId = reviewId, IsSelected = expected.IsSelected };
+
+            this.repository.Setup(x => x.All()).Returns(reviews.BuildMock());
+
+            this.filterCourse
+               .Setup(x => x.GetCourseByIdAsync<BaseCourseViewModel>(It.IsAny<int>()))
+               .ReturnsAsync(new BaseCourseViewModel { Id = expected.CourseId });
+
+            var service = new ReviewService(this.repository.Object, this.filterCourse.Object, this.Mapper);
+            await service.Selecting(model);
+
+            Assert.False(expected.IsSelected);
+            this.repository.Verify(m => m.All(), Times.Once());
+            this.repository.Verify(m => m.SaveChangesAsync(), Times.Once());
+        }
+
+        [Fact]
+        public async Task SelectingReviewByCouseDoesNotExistShouldThrowsException()
+        {
+            this.filterCourse
+               .Setup(x => x.GetCourseByIdAsync<BaseCourseViewModel>(It.IsAny<int>()))
+               .ReturnsAsync((BaseCourseViewModel)null);
+
+            var service = new ReviewService(this.repository.Object, this.filterCourse.Object, this.Mapper);
+            var model = new SelectingReviewViewModel();
+
+            var ex = await Assert.ThrowsAsync<ArgumentException>(async () => await service.Selecting(model));
+            Assert.Equal(GlobalExceptions.CourseDoesNotExistExceptionMessage, ex.Message);
+        }
+
+        [Theory]
+        [InlineData(10)]
+        public async Task SelectingReviewByCourseShouldThrowsExceptionIfSelectedReviewsAreMoreThan5(int reviewId)
+        {
+            var reviews = GetReviews().Where(x => x.CourseId == 5);
+            var expected = reviews.FirstOrDefault(x => x.Id == reviewId);
+            var model = new SelectingReviewViewModel() { CourseId = expected.CourseId, ReviewId = reviewId, IsSelected = expected.IsSelected };
+
+            this.repository.Setup(x => x.All()).Returns(reviews.BuildMock());
+
+            this.filterCourse
+               .Setup(x => x.GetCourseByIdAsync<BaseCourseViewModel>(It.IsAny<int>()))
+               .ReturnsAsync(new BaseCourseViewModel { Id = expected.CourseId });
+
+            var service = new ReviewService(this.repository.Object, this.filterCourse.Object, this.Mapper);
+
+            var ex = await Assert.ThrowsAsync<ArgumentOutOfRangeException>(async () => await service.Selecting(model));
+            Assert.Equal(GlobalExceptions.LimitOfSelectedReviews, ex.ParamName);
+        }
+
+        [Theory]
+        [InlineData(-1, 5)]
+        public async Task SelectingReviewByCourseShouldThrowsExceptionIfReviewDoesNotExist(int reviewId, int courseId)
+        {
+            var reviews = GetReviews().Where(x => x.CourseId == courseId);
+            var model = new SelectingReviewViewModel() { CourseId = courseId, ReviewId = reviewId };
+
+            this.repository.Setup(x => x.All()).Returns(reviews.BuildMock());
+
+            this.filterCourse
+               .Setup(x => x.GetCourseByIdAsync<BaseCourseViewModel>(It.IsAny<int>()))
+               .ReturnsAsync(new BaseCourseViewModel { Id = courseId });
+
+            var service = new ReviewService(this.repository.Object, this.filterCourse.Object, this.Mapper);
+
+            var ex = await Assert.ThrowsAsync<ArgumentException>(async () => await service.Selecting(model));
             Assert.Equal(GlobalExceptions.DoesNotExistReview, ex.Message);
         }
 
@@ -140,8 +369,6 @@
                 .Setup(x => x.GetCourseByIdAsync<BaseCourseViewModel>(It.IsAny<int>())).ReturnsAsync(new BaseCourseViewModel { Id = 1 });
         }
 
-
-
         private static List<Review> GetReviews()
         {
             return new List<Review>()
@@ -152,8 +379,18 @@
                 new Review { Id = 4, Content = "Test", CourseId = 3, Rating = 6, User = new ApplicationUser() },
                 new Review { Id = 5, Content = "Test", CourseId = 4, Rating = 2, User = new ApplicationUser() },
                 new Review { Id = 6, Content = "Test", CourseId = 4, Rating = 1, User = new ApplicationUser() },
-                new Review { Id = 7, Content = "SELECTED", CourseId = 5, Rating = 6, IsSelected = true, User = new ApplicationUser() },
-                new Review { Id = 8, Content = "SELECTED", CourseId = 5, Rating = 6, IsSelected = true, User = new ApplicationUser() },
+                new Review { Id = 7, Content = "SELECTED", CourseId = 5, Rating = 6, IsSelected = false, User = new ApplicationUser() },
+                new Review { Id = 8, Content = "SELECTED", CourseId = 5, Rating = 6, IsSelected = false, User = new ApplicationUser() },
+                new Review { Id = 9, Content = "SELECTED", CourseId = 5, Rating = 6, IsSelected = false, User = new ApplicationUser() },
+                new Review { Id = 10, Content = "SELECTED", CourseId = 5, Rating = 6, IsSelected = false, User = new ApplicationUser() },
+                new Review { Id = 11, Content = "SELECTED", CourseId = 5, Rating = 6, IsSelected = false, User = new ApplicationUser() },
+                new Review { Id = 12, Content = "SELECTED", CourseId = 5, Rating = 6, IsSelected = false, User = new ApplicationUser() },
+                new Review { Id = 13, Content = "SELECTED", CourseId = 5, Rating = 6, IsSelected = true, User = new ApplicationUser() },
+                new Review { Id = 14, Content = "SELECTED", CourseId = 5, Rating = 6, IsSelected = true, User = new ApplicationUser() },
+                new Review { Id = 15, Content = "SELECTED", CourseId = 5, Rating = 6, IsSelected = true, User = new ApplicationUser() },
+                new Review { Id = 17, Content = "SELECTED", CourseId = 5, Rating = 6, IsSelected = true, User = new ApplicationUser() },
+                new Review { Id = 18, Content = "SELECTED", CourseId = 5, Rating = 6, IsSelected = true, User = new ApplicationUser() },
+                new Review { Id = 19, Content = "SELECTED", CourseId = 5, Rating = 6, IsSelected = true, User = new ApplicationUser() },
             };
         }
     }
