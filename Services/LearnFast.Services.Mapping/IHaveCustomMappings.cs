b12fa4d15cef2c5244b581902dc56cdba767bdb9
George <p3trovsit@gmail.com>
2022-12-14 11:56:08 +0200

    Update main

--- Services/LearnFast.Services.Mapping/IHaveCustomMappings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LearnFast.Services.Mapping
-{
-    using AutoMapper;
-
-    public interface IHaveCustomMappings
-    {
-        void CreateMappings(IProfileExpression configuration);
-    }
-}
