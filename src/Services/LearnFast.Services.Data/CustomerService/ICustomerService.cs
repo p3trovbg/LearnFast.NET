73f700eddb1051a0badfc56a6f43d897a5d443ba
George <p3trovsit@gmail.com>
2022-12-24 23:59:46 +0200

    Payment

---
+++ src/Services/LearnFast.Services.Data/CustomerService/ICustomerService.cs
@@ -0,0 +1,11 @@
+﻿namespace LearnFast.Services.Data.CustomerService
+{
+    using System.Threading.Tasks;
+
+    public interface ICustomerService
+    {
+        public Task<string> CreateCustomerAsync(string email);
+
+        public Task<string> GetCustomerByEmailAsync(string email);
+    }
+}
---
+++ src/Web/LearnFast.Web/Controllers/PaymentController.cs
@@ -0,0 +1,63 @@
+﻿namespace LearnFast.Web.Controllers
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    using Microsoft.Extensions.Configuration;
+    using Stripe;
+
+    public class PaymentController : BaseController
+    {
+        private readonly IConfiguration configuration;
+
+        public PaymentController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProcessPayment(int amount, string payerCustomerId, string recipientCustomerId)
+        {
+            try
+            {
+                // Set the API key for Stripe
+                StripeConfiguration.ApiKey = "sk_test_1234567890";
+
+                // Create a charge object to represent the payment
+                var chargeOptions = new ChargeCreateOptions
+                {
+                    Amount = amount,
+                    Currency = "usd",
+                    Customer = payerCustomerId,
+                    TransferData = new ChargeTransferDataOptions
+                    {
+                        Destination = recipientCustomerId,
+                    },
+                };
+
+                // Initiate the payment
+                var chargeService = new ChargeService();
+                Charge charge = await chargeService.CreateAsync(chargeOptions);
+
+                // Payment was successful
+                return RedirectToAction("PaymentSuccess");
+            }
+            catch (StripeException ex)
+            {
+                // There was an error processing the payment
+                return RedirectToAction("PaymentError", new { error = ex.Message });
+            }
+        }
+
+        public IActionResult PaymentSuccess()
+        {
+            return this.View();
+        }
+
+        public IActionResult PaymentError(string error)
+        {
+            this.ViewBag.Error = error;
+            return this.View();
+        }
+    }
+}
